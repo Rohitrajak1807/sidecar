ab017f9a15b2692f2c38f860181d87052927dc0a
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-06-11 12:10:44 +0530

    using .env file not using docker build

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DOCKER_GATEWAY_HOST=172.17.0.1
-DOCKER_BUILDKIT=1 docker build --add-host=host.docker.internal:${DOCKER_GATEWAY_HOST} --secret id=user_name,src=./.user --secret id=pass,src=./.pass -f Dockerfile
